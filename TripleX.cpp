47e9dc5a932a907219f1506f8e35418bbbb0428e
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-10-30 15:56:13 -0300

    [feat][#cpp]: Declaração de variáveis, console print, include tag.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,3 +1,24 @@
+#include <iostream>
+
 int main() {
+    std::cout << "You just woke up into a very strange room...";
+    std::cout << std::endl;
+    std::cout << "There's a door not so far away from where you are.";
+    std::cout << std::endl;
+    std::cout << "As you can see, a lock keeps the door safe and sound.";
+    std::cout << std::endl;
+    std::cout << "You might want to descovery what's behinde it...";
+    std::cout << std::endl;
+
+    int a = 4;
+    int b = 3;
+    int c = 2;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
     return 0;
 }
