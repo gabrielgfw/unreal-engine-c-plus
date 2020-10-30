cf7171c7e5a1661276fe54a3703d11f3e393d965
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-10-30 18:25:43 -0300

    [feat][#cpp]: Segregando o código em funções.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 
-int main() {
-    std::cout << "-------------------------------------" << std::endl;
-    std::cout << "You just woke up into a very strange room..." << std::endl;
-    std::cout << "There's a door not so far away from where you are." << std::endl;
-    std::cout << "As you can see, a lock keeps the door safe and sound." << std::endl;
-    std::cout << "You might want to descovery what's behinde it..." << std::endl;
-    std::cout << "-------------------------------------" << std::endl;
+void PrintIntroduction() {
+
+    std::cout << "------------------------------------- \n";
+    std::cout << "You just woke up into a very strange room... \n";
+    std::cout << "There's a door not so far away from where you are. \n";
+    std::cout << "As you can see, a lock keeps the door safe and sound. \n";
+    std::cout << "You might want to descovery what's behinde it... \n";
+    std::cout << "------------------------------------- \n";
+
+}
+
+void PlayGame() {
+
+    PrintIntroduction();
 
     const int CodeA = 4;
     const int CodeB = 3;
@@ -15,12 +22,13 @@ int main() {
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << "-------------------------------------" << std::endl;
-    std::cout << "There are 3 numbers in the lock's code." << std::endl;
-    std::cout << "A little papper near the door, glued at the wall says:" << std::endl;
-    std::cout << "-------------------------------------" << std::endl;
+    std::cout << "-------------------------------------\n";
+    std::cout << "There are 3 numbers in the lock's code.\n";
+    std::cout << "A little papper near the door, glued at the wall says:\n";
+    std::cout << "-------------------------------------\n";
     std::cout << "+ The sum of the 3 numbers combination results in: " << CodeSum << std::endl;
-    std::cout << "+ If you multiply all numbers, the result will be: " << CodeProduct << std::endl;    std::cout << "-------------------------------------" << std::endl;
+    std::cout << "+ If you multiply all numbers, the result will be: " << CodeProduct << std::endl;
+    std::cout << "-------------------------------------\n";
  
     int GuessA, GuessB, GuessC;
     std::cin >> GuessA;
@@ -32,10 +40,18 @@ int main() {
     int GuessProduct = GuessA * GuessB * GuessC;  
 
     if(GuessSum == CodeSum && GuessProduct == CodeProduct) {
-        std::cout << "You win!";
+        std::cout << "\nYou win!\n";
     } else {
-        std::cout << "Wrong Answer!";
+        std::cout << "\nWrong Answer!\n";
     }
+}
 
+int main() {
+
+    while(true) {
+        PlayGame();
+        std::cin.clear();  // Clear any erros.
+        std::cin.ignore(); // Discards the buffer.
+    }
     return 0;
 }
